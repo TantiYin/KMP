e7e80fdf2a27430d4ea4e0026b74a9cb6580c5b3
tantiyin <iyintao@gmail.com>
2016-09-12 11:20:52 +0800

    命名规范

--- kmp.cpp
+++ kmp.cpp
@@ -5,71 +5,71 @@ using namespace std;
 
 int gStrLen = 0;
 
-bool strNext(char* sStr, int* pNext)
+bool StrNext(char* sStr, int* pNext)
 {
-	pNext[0] = -1;
-	pNext[1] = 0;
-	int iMaxLen = 0;
-	for (int i = 2; i < gStrLen; ++i)
-	{
-		while ((iMaxLen >= 0) && (sStr[i-1] != sStr[iMaxLen]))
-		{
-			iMaxLen = pNext[iMaxLen];
-		}
+    pNext[0] = -1;
+    pNext[1] = 0;
+    int iMaxLen = 0;
+    for (int i = 2; i < gStrLen; ++i)
+    {
+        while ((iMaxLen >= 0) && (sStr[i-1] != sStr[iMaxLen]))
+        {
+            iMaxLen = pNext[iMaxLen];
+        }
 
-		iMaxLen++;		
-		pNext[i] = iMaxLen;
-	}
-	return true;
+        iMaxLen++;        
+        pNext[i] = iMaxLen;
+    }
+    return true;
 
 }
 
-void match(char* sStr1,char* sStr2,int* pNext)
+void Match(char* sStr1,char* sStr2,int* pNext)
 {
-	int iLen = strlen(sStr1);
-	int i = 0, j = 0;
-	while ((i < gStrLen) && (j < iLen))
-	{
-		if (i < 0 || sStr1[j] == sStr2[i])
-		{
-			i++;
-			j++;
-		} 
-		else
-		{
-			i = pNext[i];
-		}
-	}
-	if (i == gStrLen)
-	{
-		cout << "successful" << endl;
-		cout << "pos:" << j - i << endl;
-	} 
-	else
-	{
-		cout << "failed" << endl;
-	}
+    int iLen = strlen(sStr1);
+    int i = 0, j = 0;
+    while ((i < gStrLen) && (j < iLen))
+    {
+        if (i < 0 || sStr1[j] == sStr2[i])
+        {
+            i++;
+            j++;
+        } 
+        else
+        {
+            i = pNext[i];
+        }
+    }
+    if (i == gStrLen)
+    {
+        cout << "successful" << endl;
+        cout << "pos:" << j - i << endl;
+    } 
+    else
+    {
+        cout << "failed" << endl;
+    }
 }
 
 int main(int argc,char** argv)
 {
-	int * pNext;
-	char* sStr = "abcabc";
-	char sStr2[100];
+    int * pNext;
+    char* sStr = "abcabc";
+    char sStr2[100];
 
-	gStrLen = strlen(sStr);
-	pNext = new int[gStrLen];
-	strNext(sStr, pNext);
+    gStrLen = strlen(sStr);
+    pNext = new int[gStrLen];
+    StrNext(sStr, pNext);
 
-	for (int i = 0; i < gStrLen; ++i)
-	{
-		cout << pNext[i] << " ";
-	}
-	cout << endl;
+    for (int i = 0; i < gStrLen; ++i)
+    {
+        cout << pNext[i] << " ";
+    }
+    cout << endl;
 
-	cin >> sStr2;
-	match(sStr2, sStr, pNext);
+    cin >> sStr2;
+    Match(sStr2, sStr, pNext);
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
